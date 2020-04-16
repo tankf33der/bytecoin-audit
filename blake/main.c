ea914f0b49ecc7091bc89c5bbcec6c68fb8a872d
Mike <tankf33der@disroot.org>
2020-04-16 08:56:21 +0300

    .

--- blake/main.c
+++ blake/main.c
@@ -2,12 +2,18 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <inttypes.h>
+#include <string.h>
 #include "blake256.h"
 
 int main(void)
 {
     printf("start\n");
-
+    uint8_t r[32];
+    uint8_t m[512];
+    for (size_t i = 0; i < 512; i++) {
+        m[i] = i;
+    }
+    blake256_hash(r, m, 0);
 
     return 0;
 }
